a8b61d96ca44c539258c88a92382d9d8442dd024
Pranav <pranava880@gmail.com>
2025-04-29 13:37:28 -0400

    feat: trying sliders

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from agent import llm, template
 
 st.title('Welcome to the APP')
 work = st.text_area('Enter work that needs to be completed as well as its estimated time needed to complete')
-time = st.text_input('Enter total time available for studying per day')
-day = st.text_input('Enter total days available to study')
+time = st.slider('Enter total time available for studying per day')
+day = st.slider('Enter total days available to study')
 
 button = st.button('Generate Study Plan')
 
