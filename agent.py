c18b9f1c97f6bbdaf99d232586d46f8753d7917c
Pranav <pranava880@gmail.com>
2025-04-29 00:13:36 -0400

    feat: updated chat ui and flow

--- agent.py
+++ agent.py
@@ -7,11 +7,11 @@ import getpass
 if "OPENAI_API_KEY" not in os.environ:
     os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter your OpenAI API key: ")
 
-model = ChatOpenAI(model="gpt-4o-mini", temperature=0.0)
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.0)
 
 template = ChatPromptTemplate.from_messages(
     [
-        ("system", "You are a helpful assistant. Your name is Bob"),
-        ("user", "{input}"),
+        ("system", "You are an AI agent designed to assist users in organizing their coursework and generating personalized study plans."),
+        ("user", "This is the work I have to do: {work}. This is the total amount of time I have to complete things per day: {time}. This is the total amount of days I have to complete the tasks: {day}. Write me a day by day study plan based on these parameters."),
     ]
 )
--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 import streamlit as st
-from agent import model, template
+from agent import llm, template
 
 st.title('Welcome to the APP')
-prompt = st.text_input('Enter prompt here')
+work = st.text_area('Enter work that needs to be completed as well as its estimated time needed to complete')
+time = st.text_input('Enter total time available for studying per day')
+day = st.text_input('Enter total days available to study')
 
-if prompt:
-    formatted_prompt = template.format_messages(input=prompt)
-    result = model.invoke(formatted_prompt)
-    st.write(result.content)
+button = st.button('Generate Study Plan')
+
+if button:
+    if work and time and day:
+        formatted_prompt = template.format_messages(work=work, time=time, day=day)
+        result = llm.invoke(formatted_prompt)
+        st.write(result.content)
+    else:
+        st.write('ERROR: COMPLETE ALL FIELDS')
