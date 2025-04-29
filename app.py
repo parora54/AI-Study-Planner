c15d5668279abb3160cf37fcc75099812bbfb944
Pranav <pranava880@gmail.com>
2025-04-29 13:40:26 -0400

    fix: slider values

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from agent import llm, template
 
 st.title('Welcome to the APP')
 work = st.text_area('Enter work that needs to be completed as well as its estimated time needed to complete')
-time = st.slider('Enter total time available for studying per day')
-day = st.slider('Enter total days available to study')
+time = st.slider(label='Enter total time available for studying per day (hours)', min_value=1.0, max_value=9.0, value=4.0, step=0.5)
+day = st.slider('Enter total days available to study', min_value=1, max_value=100, value=7, step=1)
 
 button = st.button('Generate Study Plan')
 
